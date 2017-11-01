3bb2401cbb36dccffe34a4652848e2919b2a5d08
Jaroslav Vysoky <jaroslav.vysoky@gmail.com>
2017-11-01 22:38:04 +0000

    html template, .format

--- bottle_app.py
+++ bottle_app.py
@@ -3,7 +3,7 @@
 # simple bottle application
 
 from bottle import default_app, route, static_file
-import os
+import os, os.path
 
 ROOT = "/home/vysoky/chess"
 
@@ -12,39 +12,42 @@ def server_static(filepath):
     ipath = ROOT + filepath
     if os.path.isdir(ipath):
         list_dir = os.listdir(ipath)
-        output = []
-        output.append('<html>')
-        output.append('<head>')
-        output.append('<title>List of %s</title>' % filepath)
-        output.append("""
+        html_template = """
+<html>
+    <head>
+        <title>List of {path}</title>
         <style>
-            li.file {
+            li.file {{
                 list-style-type: circle;
-            }
-            li.folder {
+            }}
+            li.folder {{
                 list-style-type: square;
-            }
-        </style>""")
-        output.append('</head>')
-        output.append('<h2>Content of %s</h2>' % filepath)
-        output.append('<ul>')
+            }}
+        </style>
+    </head>
+    <body>
+        <h2>Content of {path}</h2>
+        <ul>
+{items}
+        </ul>
+    </body>
+</html>
+"""
         list_isfile = []
         # list of (<isfile>, <name>)
         for i in list_dir:
         	list_isfile.append((os.path.isfile(os.path.join(ipath,i)),i))
         # sort - folders first then sorted by names
         list_isfile.sort(key = lambda x: str(x[0]) + x[1])
+        items = ""
         for item in list_isfile:
-            output.append('<li class="{}">'.format("file" if item[0] else "folder"))
-            output.append('<a href="%(full_path)s">%(name)s</a>' % {
+            items += '<li class="{}">'.format("file" if item[0] else "folder")
+            items += ('<a href="%(full_path)s">%(name)s</a>' % {
                 'name': item[1],
                 'full_path': os.path.join(filepath, item[1])
             })
-            output.append('</li>')
-        output.append('</ul>')
-        output.append('</html>')
-
-        return "\n".join(output)
+            items += ('</li>\n')
+        return html_template.format(path=filepath, items=items)
     else:
         return static_file(filepath, root=ROOT)
 
