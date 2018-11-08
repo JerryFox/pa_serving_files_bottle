84b248b942e5b0ddfd56fc2f16a653bb26bd0805
Jaroslav Vysoký <jaroslav.vysoky@gmail.com>
2018-11-08 08:18:20 +0000

    import hiddenconfig as an option

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
-"""
 ROOT = "/home/jardacarda"   # where are files serving from
 PATH_PREFIX = "/files"      # path prefix in browser
 INTER_PATH = ""             # if app is not in the root of the hosting server
 SHOW_HIDDEN = False         # items with "hidden" prefix
-"""
-from hiddenconfig import *
+
+try:
+    from hiddenconfig import *
+except ImportError:
+    pass
